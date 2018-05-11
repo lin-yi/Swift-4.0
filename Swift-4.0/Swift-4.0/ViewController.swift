11890d318fe49ac28cfd9ff95ed6b43ae5792d2f
林一 <linyi_ios@icloud.com>
2018-05-11 16:04:15 +0800

    init

---
+++ Swift-4.0/Swift-4.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-4.0
+//
+//  Created by 林一 on 2018/5/11.
+//  Copyright © 2018 linyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
